e799d09409de665c074dc46b1545406f780e0057
Michael Watson <mwatson10@gmail.com>
2019-04-02 14:32:02 -0400

    mapping

--- python-for-data-analysis/practice-python/proghist.py
+++ python-for-data-analysis/practice-python/proghist.py
@@ -12,18 +12,26 @@ def get_longitude(x):
 
 def main():
     df = pd.read_csv('census.csv', index_col=None, header=0, sep=",")
-
+    df['Country'] = "United Kingdom"
 
 
 
 
     geolocator = Nominatim()
 
+    '''
     geolocate_column = df['Area_Name'].apply(geolocator.geocode)
     df['Latitude'] = geolocate_column.apply(get_latitude)
     df['longitude'] = geolocate_column.apply(get_longitude)
+    '''
 
-    df.to_csv('output.csv')
+    df['helper'] = df['Area_Name'].map(str) + " " + df['Country'].map(str)
+    geolocate_column = df['helper'].apply(geolocator.geocode)
+    df['latitude'] = geolocate_column.apply(get_latitude)
+    df['longitude'] = geolocate_column.apply(get_longitude)
+
+
+    df.to_csv('output_2.csv')
 
 
 if __name__ == '__main__':
